bcea32f5ae3d58def31eb5cc0b59b21915e50f67
ChiaYu Tung <joeydong@Joey-Tungs-MBP-M2.local>
2025-11-13 02:03:58 +0800

    test: import solutions by file path to avoid package path issues

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,8 +1,10 @@
+import importlib.util
+import pathlib
 
-import importlib, glob
-
-folders = glob.glob('problems/*/solution.py')
 def test_import_all_solutions():
-    for path in folders:
-        mod = path.replace('/', '.').rstrip('.py')
-        importlib.import_module(mod)
+    for path in pathlib.Path("problems").rglob("solution.py"):
+        mod_name = f"solutions.{path.parent.name}"
+        spec = importlib.util.spec_from_file_location(mod_name, path)
+        mod = importlib.util.module_from_spec(spec)
+        assert spec and spec.loader
+        spec.loader.exec_module(mod)
